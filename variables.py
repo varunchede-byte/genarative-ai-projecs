61678d0f53dedb928d7f88772d557f10f2adde19
varunchede-byte <varunchede@gmail.com>
2026-07-12 11:31:00 +0530

    variables.py

---
+++ variables.py
@@ -0,0 +1,17 @@
+name ="varun"
+age = 25
+percentage = 12.5
+isCorrect = True
+
+print(f"{name} - {age} - {percentage} - {isCorrect} ")
+print(name )
+print(age )
+print(percentage )
+if age is 25:
+    print("is True")
+else:
+    print("is False")
+
+
+
+    
